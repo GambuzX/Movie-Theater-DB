dbd816b1d18f49d68caba3d9ed5598bf38321b2f
GambuzX <mg_mesquita@hotmail.com>
2019-05-22 16:10:45 +0100

    Started working on third trigger

--- gatilho3_adiciona.sql
+++ gatilho3_adiciona.sql
@@ -1,1 +1,12 @@
 PRAGMA foreign_keys = ON;
+
+CREATE TRIGGER DefinePrecoEfetivo
+AFTER INSERT ON Pedido
+FOR EACH ROW
+BEGIN
+    UPDATE Pedido
+    SET precoEfetivo=precoOriginal * (1 - (SELECT desconto
+                                            FROM Pedido P JOIN Cliente C ON P.cliente=C.pessoaID
+                                            WHERE P.pedidoID=NEW.pedidoID)) / 100
+    WHERE Pedido.pedidoID=NEW.pedidoID;
+END;
--- gatilho3_verifica.sql
+++ gatilho3_verifica.sql
@@ -1,1 +1,15 @@
 PRAGMA foreign_keys = ON;
+
+
+
+SELECT precoOriginal as "Preco Original", desconto as "Desconto", precoEfetivo as "Preco Efetivo Antes" 
+FROM Pedido P INNER JOIN Cliente C ON P.cliente=C.pessoaID
+UNION ALL
+SELECT " ", " ", " ";
+
+
+
+SELECT precoOriginal as "Preco Original", desconto as "Desconto", precoEfetivo as "Preco Efetivo Depois"
+FROM Pedido P INNER JOIN Cliente C ON P.cliente=C.pessoaID
+UNION ALL
+SELECT " ", " ", " ";
