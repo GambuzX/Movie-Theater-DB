57b235d8bc627b11bc2edacfc39b0b5d5c2dc3c2
Gustavo Torres <torresgustam@gmail.com>
2019-05-22 15:38:49 +0100

    One more query

--- int9.sql
+++ int9.sql
@@ -3,10 +3,7 @@
 .nullvalue NULL
 
 /*Estatisticas realtivamente ao preço original e ao preço efetivos, diferença, percentagem,entre outros.*/
-/*so dos descontados*/
-
-Select SUM(precoOriginal) as 'Dinheiro Total ' , SUM(precoEfetivo) as  'Dinheiro Efetivo' , 100-100*SUM(precoEfetivo)/SUM(precoOriginal) as 'Percentagem De Dinheiro Descontado'
- from Pedido;
+/*so dos descontados*/ 
 
 Select SUM(precoOriginal) as 'Dinheiro Total ' , SUM(precoEfetivo) as  'Dinheiro Efetivo' , 100-100*SUM(precoEfetivo)/SUM(precoOriginal) as 'Percentagem De Dinheiro Descontado' , MAX(tipo) as 'Adesao que mais usufruiu'
   from Pedido JOIN Membro JOIN Adesao on Pedido.cliente = Membro.clienteID AND Membro.adesao = Adesao.adesaoID;
