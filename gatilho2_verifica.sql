819b1e1e4e3e25c464bfb500e17109c40136bfd4
GambuzX <mg_mesquita@hotmail.com>
2019-05-22 15:44:23 +0100

    Trigger 2

--- gatilho2_verifica.sql
+++ gatilho2_verifica.sql
@@ -1,1 +1,21 @@
 PRAGMA foreign_keys = ON;
+.headers on
+.mode columns
+.nullvalue NULL
+
+INSERT INTO Pessoa (pessoaID, nome,NIF,telefone,dataNascimento , morada , codigoPostal) values (100, 'Idoso' , 259561924 , 9322024419 , '1970-03-08' , 'Rua Indefinida' , '4420-732');
+INSERT INTO Pessoa (pessoaID, nome,NIF,telefone,dataNascimento , morada , codigoPostal) values (101, 'Estudante' , 254011924 , 9322024419 , '1970-03-08' , 'Rua Indefinida' , '4420-732');
+INSERT INTO Pessoa (pessoaID, nome,NIF,telefone,dataNascimento , morada , codigoPostal) values (102, 'Crianca' , 291001924 , 9322024419 , '1970-03-08' , 'Rua Indefinida' , '4420-732');
+
+INSERT INTO Cliente (pessoaID) values (100);
+INSERT INTO Cliente (pessoaID) values (101);
+INSERT INTO Cliente (pessoaID) values (102);
+
+SELECT nome as Pessoa, desconto as "Desconto antes" FROM Cliente C NATURAL JOIN Pessoa P UNION ALL SELECT " ", " ";
+
+INSERT INTO Membro (clienteID , email,numeroCartao,validadeAdesao,adesao) values (100 , 'generico1@mail.com' , 2 , '2020-04-09' , 3);
+INSERT INTO Membro (clienteID , email,numeroCartao,validadeAdesao,adesao) values (101 , 'generico2@mail.com' , 3 , '2019-11-05' , 5);
+INSERT INTO Membro (clienteID , email,numeroCartao,validadeAdesao,adesao) values (102 , 'generico3@mail.com' , 4 , '2020-09-17' , 2);
+
+
+SELECT nome as Pessoa, desconto as "Desconto depois" FROM Cliente C NATURAL JOIN Pessoa P UNION ALL SELECT " ", " ";
