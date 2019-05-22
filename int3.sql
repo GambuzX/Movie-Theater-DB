d6c975bb20ed135184de5d3dba951b6d6cbfa987
GambuzX <mg_mesquita@hotmail.com>
2019-05-22 10:00:47 +0100

    Simplified query 3 with a View

--- int3.sql
+++ int3.sql
@@ -2,36 +2,35 @@
 .headers on
 .nullvalue NULL
 
+DROP VIEW IF EXISTS FilmePorPedido;
+
 /* 
 Rendimento total dos 10 melhores filmes durante determinado periodo de tempo
 bilhetes + produtos
 */
+
+CREATE VIEW FilmePorPedido AS
+SELECT B.pedido as pedidoID, S.filme as filmeID
+FROM Bilhete B JOIN Sessao S ON B.sessao=S.sessaoID
+WHERE date(S.horaInicio) >= '2019-04-29' AND date(S.horaInicio) <= '2019-06-23';
  
 SELECT nome as "Filme", (SELECT IFNULL(SUM(val), 0)
                             FROM
                                 -- bilhetes 
                                 (SELECT P.precoEfetivo as val
                                 FROM Pedido P
-                                WHERE P.pedidoID in (SELECT B.pedido
-                                                    FROM Bilhete B INNER JOIN Sessao S INNER JOIN Filme F
-                                                    WHERE B.sessao=S.sessaoID
-                                                    AND S.filme=F.filmeID
-                                                    AND F.nome=filme.nome 
-                                                    AND date(S.horaInicio) >= '2019-04-29' 
-                                                    AND date(S.horaInicio) <= '2019-06-23')
+                                WHERE P.pedidoID in (SELECT pedidoID
+                                                    FROM FilmePorPedido FPP JOIN Filme
+                                                    ON FPP.filmeID=F.filmeID)
                                 UNION ALL
                                 -- produtos
                                 SELECT PA.quantidade*Prod.preco as val
                                     FROM Produto Prod INNER JOIN ProdutoAdquirido PA INNER JOIN Pedido P
                                         WHERE Prod.produtoID=PA.produto
                                         AND PA.pedido=P.pedidoID
-                                        AND P.pedidoID in (SELECT B.pedido
-                                                            FROM Bilhete B INNER JOIN Sessao S INNER JOIN Filme F
-                                                            WHERE B.sessao=S.sessaoID
-                                                            AND S.filme=F.filmeID
-                                                            AND F.nome=filme.nome 
-                                                            AND date(S.horaInicio) >= '2019-04-29' 
-                                                            AND date(S.horaInicio) <= '2019-06-23'))) as "Rendimento Total"
-FROM Filme filme
+                                        AND P.pedidoID in (SELECT pedidoID
+                                                            FROM FilmePorPedido FPP JOIN Filme
+                                                            ON FPP.filmeID=F.filmeID))) as "Rendimento Total"
+FROM Filme F
 ORDER BY "Rendimento Total" DESC
 LIMIT 10;
--- query_tester.sql
+++ query_tester.sql
@@ -1,4 +1,4 @@
 .read criar.sql
 .read povoar.sql
 
-.read int5.sql
+.read int3.sql
