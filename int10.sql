7a72a6865c23bac8832adfc05c5267c4d9c5960b
leonardofmoura <up201706907@fe.up.pt>
2019-05-26 20:42:28 +0100

    added int 10

--- int10.sql
+++ int10.sql
@@ -2,6 +2,23 @@
 .headers on
 .nullvalue NULL
 
+DROP VIEW IF EXISTS MelhoresLugaresSala;
+DROP VIEW IF EXISTS MelhoresLugaresSalaCin;
+
 /*Selecionar lugares preferidos por cada sala por cada cinema*/
+/*Lugares com mais bilhetes comprados */
 
 /*Limit OFFSET || */
+
+CREATE VIEW MelhoresLugaresSala AS 
+SELECT BilheteLugar.sala, BilheteLugar.fila, BilheteLugar.numero, COUNT(pedido) AS NumPedidos
+ from (bilhete INNER JOIN lugar ON bilhete.lugar = lugar.lugarID) AS BilheteLugar 
+ GROUP BY BilheteLugar.lugarID ORDER BY NumPedidos DESC; 
+
+CREATE VIEW MelhoresLugaresSalaCin AS
+SELECT * FROM  (SELECT cinema.nome,cinema.cinemaID FROM cinema) as Cin
+INNER JOIN (( SELECT cinema,salaID AS NumSala FROM sala) AS SalaRec
+ INNER JOIN MelhoresLugaresSala ON SalaRec.NumSala = MelhoresLugaresSala.sala) AS SalaCin ON Cin.cinemaID = SalaCin.cinema
+ ORDER BY MelhoresLugaresSala.sala DESC;  
+
+SELECT nome,NumSala,fila,numero,NumPedidos FROM MelhoresLugaresSalaCin;
--- int7.sql
+++ int7.sql
@@ -2,7 +2,7 @@
 .headers on
 .nullvalue NULL
 
-/* 10 clientes mais fieis LIMIT 10 (ou 5) (aqueles que foram mais vezes i.e fizeram mais pedidos) */
+/* 10 clientes mais fieis por cinema LIMIT 10 (ou 5) (aqueles que foram mais vezes i.e fizeram mais pedidos) */
 
 SELECT PedidosPessoaCliente.nome, COUNT(PedidosPessoaCliente.pedidoID) AS NumeroDePedidos
 FROM ((Pedido INNER JOIN Cliente ON Pedido.cliente = Cliente.pessoaID) AS PedidoCliente 
