94f1c4765cc5bf175d895435832734fd8c8346ec
GambuzX <mg_mesquita@hotmail.com>
2019-05-26 23:25:10 +0100

    query tester to test everything

--- query_tester.sql
+++ query_tester.sql
@@ -1,4 +1,56 @@
 .read criar.sql
 .read povoar.sql
 
+.read int1.sql
+
+SELECT " ";
+
+.read int2.sql
+
+SELECT " ";
+
+.read int3.sql
+
+SELECT " ";
+
+.read int4.sql
+
+SELECT " ";
+
+.read int5.sql
+
+SELECT " ";
+
+.read int6.sql
+
+SELECT " ";
+
+.read int7.sql
+
+SELECT " ";
+
+.read int8.sql
+
+SELECT " ";
+
 .read int9.sql
+
+SELECT " ";
+
+.read int10.sql
+
+.read gatilho1_adiciona.sql
+.read gatilho1_verifica.sql
+.read gatilho1_remove.sql
+
+SELECT " ";
+
+.read gatilho2_adiciona.sql
+.read gatilho2_verifica.sql
+.read gatilho2_remove.sql
+
+SELECT " ";
+
+.read gatilho3_adiciona.sql
+.read gatilho3_verifica.sql
+.read gatilho3_remove.sql
